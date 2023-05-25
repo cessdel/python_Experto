c32abb22b3fdf7612554bf3274db9c2b9b51e252
cessdel <cesardelgadocardona@gmail.com>
2023-05-25 11:42:44 -0600

    Captura de foto

--- BD/conn.py
+++ BD/conn.py
@@ -26,17 +26,24 @@ edad = int(input("Ingrese la edad: "))
 cursor.execute("INSERT INTO usuarios (nombre, edad) VALUES (?, ?)", (nombre, edad))
 usuario_id = cursor.lastrowid  # Obtener el ID del usuario insertado
 
-# Capturar foto con la cámara
+# Inicializar la cámara
 cam = cv2.VideoCapture(0)
-ret, frame = cam.read()
+
+# Mostrar la imagen en un frame hasta que se tome la foto
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('Captura de foto', frame)
+    
+    # Esperar hasta que se presione la tecla 's' para tomar la foto
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        # Guardar la foto en la carpeta 'images'
+        foto_path = f'images/{nombre}.jpg'
+        cv2.imwrite(foto_path, frame)
+        break
+
+# Liberar la cámara y destruir las ventanas
 cam.release()
-
-# Guardar la foto en la carpeta 'images'
-foto_path = f'images/{nombre}.jpg'
-cv2.imwrite(foto_path, frame)
-
-# Actualizar el registro del usuario con la ruta de la foto
-cursor.execute("UPDATE usuarios SET foto = ? WHERE id = ?", (foto_path, usuario_id))
+cv2.destroyAllWindows()
 
 # Guardar los cambios
 conn.commit()
